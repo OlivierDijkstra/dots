722f3b11957dd2ed71904cb23b6d0fe448b8edd1
Olivier <olivierdijkstra@gmail.com>
2023-04-03 21:34:50 +0200

    Remove .config/scripts/install_vim_plug.sh

--- dot_config/scripts/executable_install_vim_plug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
