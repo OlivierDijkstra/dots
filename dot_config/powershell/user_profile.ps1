da81a92a73d721ef152f26214c2634a610d70aa4
Olivier Dijkstra <olivierdijkstra@gmail.com>
2024-05-29 19:50:00 +0200

    update

---
+++ dot_config/powershell/user_profile.ps1
@@ -0,0 +1,7 @@
+# Prompt
+oh-my-posh init pwsh --config 'C:\Users\Oli\.config\powershell\pure.json' | Invoke-Expression
+
+# Aliases
+Set-Alias nv nvim -Force
+Set-Alias ll walk
+Set-Alias j autojump
